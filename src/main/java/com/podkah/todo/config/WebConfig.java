054db6def8f430ba57d5e30cc14c00707f6a352b
hm <hm.dev7@gmail.com>
2019-03-23 10:23:08 +0700

    View Resolver and View. Spring MVC Request Processing

--- src/main/java/com/podkah/todo/config/WebConfig.java
+++ src/main/java/com/podkah/todo/config/WebConfig.java
@@ -1,11 +1,25 @@
 package com.podkah.todo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.podkah.todo")
 public class WebConfig {
+
+  public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
+  public static final String RESOLVER_SUFFIX = ".jsp";
+
+  @Bean
+  public ViewResolver viewResolver() {
+    var viewResolver = new InternalResourceViewResolver();
+    viewResolver.setPrefix(RESOLVER_PREFIX);
+    viewResolver.setSuffix(RESOLVER_SUFFIX);
+    return viewResolver;
+  }
 }
---
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>To-Do</title>
+</head>
+<body>
+ <header>
+     <nav>
+         <p>Navbar here</p>
+     </nav>
+ </header>
+ <main>
+     <article>
+         <section>
+             <h1>Hello, Spring Web MVC!</h1>
+         </section>
+     </article>
+ </main>
+</body>
+</html>
